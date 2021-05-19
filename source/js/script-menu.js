09423ade8c4aee85de8ed42351c9202e13e86c13
TamaraLavrina <tamara.lavrina@gmail.com>
2021-05-19 15:30:30 +0300

    доделки

--- source/js/script-menu.js
+++ source/js/script-menu.js
@@ -1,5 +1,6 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle-navigate');
+
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
@@ -12,17 +13,3 @@ navToggle.addEventListener('click', function () {
   }
 });
 
-
-const buyButton = document.querySelector(".product-special__button");
-const cartPopup = document.querySelector(".modal");
-const cartAdd = cartPopup.querySelector(".modal__button");
-
-buyButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.add("modal-show");
-})
-
-cartAdd.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  cartPopup.classList.remove("modal-show");
-});
